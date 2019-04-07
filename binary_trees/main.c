bc6f54a89930439b21419f286c415c9b1affae88
Anon127 <ccc47@sfu.ca>
2019-04-06 18:29:17 -0700

    Some basic info added

---
+++ binary_trees/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+
+
+
+	
+	return 0;
+}
+
+/* Info about trees in general: 
+
+Traversal and evaluation:
+	-post-order = left -> right -> self (most common for expression trees)
+		-an expression tree is a full binary tree where an arithmetic calculation is represented
+
+	-pre-order = self -> left -> right
+	-in-order = left -> self -> right
+
+
+
+*/
